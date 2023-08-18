3a16f53033ddc4f2e9f7217494cd98ad0209a6dd
federico blautzik <fedeblau@gmail.com>
2023-08-18 09:25:14 -0300

    23

--- src/components/4/GallerySlider.js
+++ src/components/4/GallerySlider.js
@@ -4,7 +4,7 @@ import { Carousel } from 'flowbite-react';
 
 export default function GallerySlider({ images, index }) {
  
-    const reorderedImg = images.slice(index).concat(images.slice(0, index))
+    const reorderedImg = images.slice(index).concat(images.slice(0, index)).concat(images.slice(3))
 
     return (
 
