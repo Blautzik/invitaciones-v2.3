252ea0cb71f881c34d99c6b11cf2bb561aa0a867
federico blautzik <fedeblau@gmail.com>
2023-09-12 14:16:45 -0300

    ENV

--- src/pages/api/form.ts
+++ src/pages/api/form.ts
@@ -22,8 +22,8 @@ export default async function handler(
     try{
         const auth = new google.auth.GoogleAuth({
             credentials: {
-                client_email: process.env.GOOGLE_CLIENT_EMAIL,
-                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+                client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
             },
             scopes: [
                 'https://www.googleapis.com/auth/drive',
