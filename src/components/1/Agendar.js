56f3dae963d36bf6f9ae694e296996e4074048f6
federico blautzik <fedeblau@gmail.com>
2023-10-05 12:11:20 -0300

    3

--- src/components/1/Agendar.js
+++ src/components/1/Agendar.js
@@ -5,22 +5,22 @@ import { RiFacebookLine, RiTwitterLine, RiInstagramLine } from "react-icons/ri";
 import { BiCalendar, BiCalendarCheck } from "react-icons/bi";
 
 
-const Agendar = ({foto_agendar, fb_link, tw_link, ig_link}) => {
+const Agendar = ({ foto_agendar, fb_link, tw_link, ig_link }) => {
     return (
         <div className='h-screen flex flex-col items-center justify-between mb-8'>
             <div className="flex flex-col items-center h-60 justify-between mb-4">
 
-            <BiCalendar className="h-20 w-20 text-black mt-6 " />
+                <BiCalendar className="h-20 w-20 text-black mt-6 " />
 
-            <h3 className={`${comfortaa.className} text-4xl mb-4`}>Agendar</h3>
-            <a className={`${openSans.className} bg-black text-white flex justify-evenly items-center w-72 text-[14px] font-[600] px-6 py-4 rounded-full `}
-                href='https://calendar.google.com/calendar'
-                target="_blank"
+                <h3 className={`${comfortaa.className} text-4xl mb-4`}>Agendar</h3>
+                <a className={`${openSans.className} bg-black text-white flex justify-evenly items-center w-72 text-[14px] font-[600] px-6 py-4 rounded-full `}
+                    href='https://calendar.google.com/calendar'
+                    target="_blank"
                 >
-                <BiCalendarCheck className="text-[20px] " />
-                AGREGAR AL CALENDARIO
-            </a>
-                </div>
+                    <BiCalendarCheck className="text-[20px] " />
+                    AGREGAR AL CALENDARIO
+                </a>
+            </div>
             <div className="shadow-2xl mb-4">
 
                 <Image
@@ -36,27 +36,30 @@ const Agendar = ({foto_agendar, fb_link, tw_link, ig_link}) => {
                 {
                     fb_link &&
                     <div className=" border-black text-black flex items-center border rounded-full h-16 w-16">
-                    <RiFacebookLine className="text-xl m-auto" />
-                </div>
+                        <RiFacebookLine className="text-xl m-auto" />
+                    </div>
                 }
 
                 {
                     tw_link &&
                     <div className=" border-black text-black flex items-center border rounded-full h-16 w-16">
 
-                    <RiTwitterLine className="text-xl m-auto"/>
-                </div>
+                        <RiTwitterLine className="text-xl m-auto" />
+                    </div>
                 }
 
                 {
                     ig_link &&
+                        <a href={ig_link} target="_blank" >
                     <div className=" border-black text-black flex items-center border rounded-full h-16 w-16 ">
-                    <RiInstagramLine className="text-xl m-auto"/>
 
-                </div>
+                            <RiInstagramLine className="text-xl m-auto" />
+
+                    </div>
+                        </a>
                 }
             </div>
-            
+
         </div>
     )
 }
