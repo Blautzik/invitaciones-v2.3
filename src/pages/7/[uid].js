418596b84464c7a7864afde2935b7b4cc52f2d8c
federico blautzik <fedeblau@gmail.com>
2023-10-05 13:21:05 -0300

    1

--- src/pages/7/[uid].js
+++ src/pages/7/[uid].js
@@ -104,6 +104,7 @@ const Invitacion = ({ article }) => {
                                 </section>
                                 
                                 
+                                
                             }
 
 
