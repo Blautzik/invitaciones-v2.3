6b37e1e1265bd760d7d839fc7c4777d8508e4cf3
federico blautzik <fedeblau@gmail.com>
2023-11-03 14:49:35 -0300

    form update

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -36,7 +36,7 @@ export default async function handler(
             valueInputOption: 'USER_ENTERED',
             requestBody: {
                 values: [
-                    [body.name, body.email, body.phone, body.message, body.menu]
+                    [body.name, body.email, body.chicos, body.message, body.menu]
                 ]
             }
         })
