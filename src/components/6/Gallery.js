9c49938e07cc07e9dbb051a15f2ed28c95ef7a27
federico blautzik <fedeblau@gmail.com>
2023-11-06 10:03:32 -0300

    titular opcional

--- src/components/6/Gallery.js
+++ src/components/6/Gallery.js
@@ -132,6 +132,7 @@ export default function Gallery({ imagenes, titulo, videoId }) {
             <button className={`${openSans.className} bg-black text-white flex justify-evenly items-center  text-[14px] font-[600] px-4 py-3 rounded-full `} onClick={() => props.setOpenModal(undefined)}>CERRAR X</button>
           </Modal.Footer>
 
+
         </Modal>
       </>
 
