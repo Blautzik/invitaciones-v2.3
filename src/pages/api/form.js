267f1860d8c4f5247634dde4f35e6553f766c998
federico blautzik <fedeblau@gmail.com>
2023-12-14 21:21:33 -0300

    32

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -1,52 +1,42 @@
+import { google } from 'googleapis';
+import { NextResponse } from 'next/server';
 
-import { google } from 'googleapis'
-import { NextResponse } from 'next/server'
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Only POST requests are allowed' });
+  }
 
-export default async function handler(
-    req,
-    NextResponse
-){
-    if(req.method !== 'POST'){
-        return NextResponse.status( 405).send({message:'solo post request'})
-    }
-    const body = req.body
+  const body = req.body;
 
-    try{
-        const auth = new google.auth.GoogleAuth({
-            credentials: {
-                client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
-                private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-            },
-            scopes: [
-                'https://www.googleapis.com/auth/drive',
-                'https://www.googleapis.com/auth/drive.file',
-                'https://www.googleapis.com/auth/spreadsheets'
-            ]
-        })
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      },
+      scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+        'https://www.googleapis.com/auth/spreadsheets',
+      ],
+    });
 
+    const sheets = google.sheets({ auth, version: 'v4' });
 
-        const sheets = google.sheets({
-            auth, 
-            version:'v4'
-        })
+    const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: body.id,
+      range: 'A2:E2',
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [[body.name, body.email, body.chicos, body.message, body.menu]],
+      },
+    });
 
-        const response = await sheets.spreadsheets.values.append({
-            spreadsheetId: body.id,
-            range: 'A1:E1',
-            valueInputOption: 'USER_ENTERED',
-            requestBody: {
-                values: [
-                    [body.name, body.email, body.chicos, body.message, body.menu]
-                ]
-            }
-        })
-
-        return NextResponse.status(201).json({
-            data: response.data
-        })
-
-    }catch(e){
-        return NextResponse.status(e.code).send({message: e.message})
-    }
-
+    return res.status(201).json({
+      data: response.data,
+    });
+  } catch (e) {
+    console.error('Error:', e);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
