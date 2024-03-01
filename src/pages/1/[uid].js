a0b5b79dfe6b09a58228ba9e3b8d741603bea0b4
federico blautzik <fedeblau@gmail.com>
2024-03-01 08:51:10 -0300

    7987463213874

--- src/pages/1/[uid].js
+++ src/pages/1/[uid].js
@@ -176,7 +176,7 @@ const Invitacion = ({ article }) => {
                   />
                 }
                 <Footer frase_cierre={article.data.frase_cierre} sin_janos={article.data.sin_janos} />
-                <div className={`w-screen bg-violeta  h-8 text-center pt-2 text-white`}>Invitaciones Jano's </div>
+                <div className={`w-screen ${article.data.color_fondo ? `bg-[${article.data.color_fondo.toLowercase()}]`: 'bg-violeta'} h-8 text-center pt-2 text-white`}>Invitaciones Jano's </div>
               </section>
             </div>
           </main>
