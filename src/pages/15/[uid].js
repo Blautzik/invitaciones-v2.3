d9106103638bdcf52d1f21658647bf85a9bbbff8
Fedeblau <153089163+Fedeblau@users.noreply.github.com>
2024-05-27 13:26:22 -0300

    me voy a casa

--- src/pages/15/[uid].js
+++ src/pages/15/[uid].js
@@ -168,40 +168,32 @@ const Invitacion = ({ article }) => {
 
 export default Invitacion
 
-
-
 export async function getStaticProps({ params, previewData }) {
-    const { uid } = params; // Extrae uid de params
+    const { uid } = params; 
 
-    // Realiza una solicitud HTTP GET a tu endpoint de Google Apps Script API
     const response = await fetch('https://script.google.com/macros/s/AKfycbzLZe2MiUp-aeZYnkncQ3pw5SIWG-s4oU27BvlPZX5zeeOrK-tljz08MiLv1q-V1RNoIQ/exec');
 
-    // Maneja el estado de la respuesta
+
     if (!response.ok) {
         return {
             notFound: true,
         };
     }
 
-    // Analiza los datos JSON de la respuesta
     const articles = await response.json();
 
-    // Encuentra el artículo que coincida con el uid
+
     const articleData = articles.find(article => String(article.nombre) === uid);
 
-    // Si no se encuentra el artículo, retorna 404
     if (!articleData) {
         return {
             notFound: true,
         };
     }
 
-    // // Procesa los datos del artículo según sea necesario
-    // const fecha = new Date(articleData.fecha);
-    // const formattedFecha = fecha.toISOString(); 
 
     const article = {
-        uid, // Usa el uid extraído
+        uid, 
         ...articleData,
     };
 
@@ -223,17 +215,14 @@ export async function getStaticPaths() {
         };
     }
 
-    // Llama a un endpoint externo de API para obtener los posts
     const res = await fetch('https://script.google.com/macros/s/AKfycbzLZe2MiUp-aeZYnkncQ3pw5SIWG-s4oU27BvlPZX5zeeOrK-tljz08MiLv1q-V1RNoIQ/exec');
     const posts = await res.json();
 
-    // Obtén las rutas que queremos prerenderizar basadas en los posts
-    // En entornos de producción, prerenderiza todas las páginas
-    // (construcciones más lentas, pero carga inicial de página más rápida)
+
     const paths = posts.map((post) => ({
         params: { uid: String(post.nombre) }, // Asegúrate de que el parámetro uid sea una cadena
     }));
 
-    // { fallback: false } significa que otras rutas deberían devolver 404
+
     return { paths, fallback: false };
 }
