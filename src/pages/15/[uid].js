de1e1644ae18cd1473d2a5db3dcfe1b377719b7f
federico blautzik <fedeblau@gmail.com>
2024-07-05 22:28:21 -0300

    hhhhhhhhhhhhh

--- src/pages/15/[uid].js
+++ src/pages/15/[uid].js
@@ -62,8 +62,8 @@ const Invitacion = ({ article }) => {
             foto_agendar = getGoogleDriveImageUrl(article.foto_agendar)
         }
 
-        if(article.foto_regalos) {
-            foto_regalos = getGoogleDriveImageUrl(article.foto_regalos)
+        if(article['foto seccion regalos (opcional)']) {
+            foto_regalos = getGoogleDriveImageUrl(article['foto seccion regalos (opcional)'])
         }
         return (
             <>
