75d172607db2fccf2f2edbf667a11d384ac3bb38
federico blautzik <fedeblau@gmail.com>
2024-07-31 13:00:15 -0300

    321 noe

--- src/pages/15/[uid].js
+++ src/pages/15/[uid].js
@@ -107,6 +107,8 @@ const Invitacion = ({ article }) => {
                                 />
                             }
 
+                            
+
 
                             <Info
                                 article={article}
