ed09e4a7f22fcbbf7bf5d5a76e54e14e46393ccb
federico blautzik <fedeblau@gmail.com>
2024-10-10 21:56:07 -0300

    4546467978854545

--- src/components/boda/InfoCeremonia.js
+++ src/components/boda/InfoCeremonia.js
@@ -140,7 +140,7 @@ const InfoCeremonia = ({ article }) => {
                 </div>
 
 
-                <a href={article.ubicacion} target='_blank'>
+                <a href={salon.link_maps} target='_blank'>
                     <button className={`${openSans.className} flex flex-row py-4 px-8 bg-black tracking-wide text-sm rounded-full text-gray-50 text-center mb-24 max-w-[280px] font-[500]`}>
                         <PiMapPin className=' text-lg mr-1' />
                         Cómo Llegar
