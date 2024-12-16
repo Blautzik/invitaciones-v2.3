39edc5285173379523bd6260d097ab7a1e4d9f8b
Fedeblau <153089163+Fedeblau@users.noreply.github.com>
2024-12-16 20:34:15 -0300

    analitycs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
 import '../styles/accordion.css'
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
